b20fbfa15a963e4cb96ae76b6f9da0a3fc6423d7
stanislavpavlovskyi <lichfb@gmail.com>
2023-02-21 12:53:30 -0500

    Spring Core Bean Annotation

--- spring-01-Core-IoC-Bean_Annotation/src/main/java/com/cydeo/bean_annotation/config/ComputerConfig.java
+++ spring-01-Core-IoC-Bean_Annotation/src/main/java/com/cydeo/bean_annotation/config/ComputerConfig.java
@@ -3,19 +3,26 @@ package com.cydeo.bean_annotation.config;
 
 import com.cydeo.bean_annotation.casefactory.Case;
 import com.cydeo.bean_annotation.casefactory.DellCase;
+import com.cydeo.bean_annotation.monitorfactory.AcerMonitor;
 import com.cydeo.bean_annotation.monitorfactory.Monitor;
 import com.cydeo.bean_annotation.monitorfactory.SonyMonitor;
 import com.cydeo.bean_annotation.motherboardfactory.AsusMotherboard;
 import com.cydeo.bean_annotation.motherboardfactory.Motherboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ComputerConfig {
 
     @Bean
-    public Monitor monitorSony(){
-        return new SonyMonitor("25 inch Beast","Sony", 25);
+    public Monitor monitorSony() {
+        return new SonyMonitor("25 inch Beast", "Sony", 25);
+    }
+    @Bean
+    @Primary
+    public Monitor monitorAcer(){
+        return new AcerMonitor("23 inch Beast","Acer", 23);
     }
 
     @Bean
