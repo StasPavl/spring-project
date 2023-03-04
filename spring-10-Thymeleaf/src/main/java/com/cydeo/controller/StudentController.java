e0c7016c1c1fc565a3ac8d00c837da7d5e5d8c43
stanislavpavlovskyi <lichfb@gmail.com>
2023-03-03 22:55:39 -0500

    Spring Thymeleaf

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -17,8 +17,8 @@ public class StudentController {
         return "student/register";
     }
     @RequestMapping("/welcome")
-    public String welcome(@RequestParam int id){
-        System.out.println(id);
+    public String welcome(@RequestParam String name){
+        System.out.println(name);
         return "student/welcome";
     }
 }
