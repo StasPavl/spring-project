538092167e2dd2281d3f732f8be951ac00381262
stanislavpavlovskyi <lichfb@gmail.com>
2023-04-05 22:21:00 -0400

    Spring Data JPA

---
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/spring11datatablecreation/Spring11DataTableCreationApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring11datatablecreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring11DataTableCreationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring11DataTableCreationApplication.class, args);
+    }
+
+}
---
+++ spring-11-Data-TableCreation/src/test/java/com/cydeo/spring11datatablecreation/Spring11DataTableCreationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring11datatablecreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring11DataTableCreationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
