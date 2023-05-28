e8d5589b383161046c52f6e53ea699526030e2c7
Stanislav Pavlovskyi <114306595+lichfb@users.noreply.github.com>
2023-05-28 15:58:35 -0400

    Add files via upload

---
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
@@ -0,0 +1,46 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.CourseDTO;
+import com.cydeo.service.CourseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses/api/v2")
+public class CourseController_ResponseEntity {
+    private final CourseService courseService;
+
+    public CourseController_ResponseEntity(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CourseDTO>> getAllCourses(){
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("Version","Cydeo.V2")
+                .header("Operation","Get Data")
+                .body(courseService.getCourses());
+    }
+    @GetMapping("{id}")
+    public ResponseEntity<CourseDTO> getCourseById(@PathVariable("id") Long id){
+
+        return ResponseEntity
+                .ok(courseService.getCourseById(id));
+    }
+    @GetMapping("/category/{name}")
+    public ResponseEntity<List<CourseDTO>> getAllCoursesByCategory(@PathVariable("name") String name){
+        return ResponseEntity
+                .ok(courseService.getCoursesByCategory(name));
+    }
+    @PostMapping
+    public ResponseEntity<CourseDTO> createCourse(@RequestBody CourseDTO courseDTO){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("Operation","Create new course")
+                .body(courseService.createCourse(courseDTO));
+    }
+}
---
+++ spring-15-REST/src/main/java/com/cydeo/dto/CourseDTO.java
@@ -0,0 +1,21 @@
+package com.cydeo.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CourseDTO {
+
+
+    private Long id;
+
+    private String name;
+    private String category;
+    private int rating;
+    private String description;
+
+}
